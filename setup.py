cd47edb4a877b0ac04f9fa5c09e6e975951de119
Daniel Greenfeld <danny@eventbrite.com>
2014-10-11 15:23:45 -0700

    Modifications to make tagging and classifying simpler

--- setup.py
+++ setup.py
@@ -1,10 +1,35 @@
-from distutils.core import setup
-setup(name='django-filepicker',
-      version='0.2.1',
-      description='Official Filepicker Django Library',
-      author='Filepicker.io',
-      author_email='contact@filepicker.io',
-      url='http://developers.filepicker.io/',
-      packages=['django_filepicker'],
-      install_requires=['django >= 1.3','requests']
-      )
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+version = '0.2.2'
+
+if sys.argv[-1] == 'publish':
+    print("Tagging the version on github:")
+    os.system("git tag -a %s -m 'version %s'" % (version, version))
+    os.system("git push --tags")
+    sys.exit()
+
+setup(
+    name='django-filepicker',
+    version=version,
+    description='Official Filepicker Django Library',
+    author='Filepicker.io',
+    author_email='contact@filepicker.io',
+    url='http://developers.filepicker.io/',
+    packages=['django_filepicker'],
+    install_requires=['django >= 1.3','requests'],
+    license="MIT",
+    zip_safe=False,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+    ]
+)
